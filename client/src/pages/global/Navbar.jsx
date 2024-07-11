85eb22a2f3e1aa7116f08842ce6647a37322d906
PLThabangR <rakgoropothabang@gmail.com>
2024-07-11 14:56:29 +0200

    Daisy and tailwind setup

--- client/src/pages/global/Navbar.jsx
+++ client/src/pages/global/Navbar.jsx
@@ -8,6 +8,23 @@ const Navbar = () => {
  //   const dispatch = useDispatch()
   return (
     <>
+    <div className="navbar bg-base-100">
+  <div className="navbar-start">
+  <a className="btn btn-ghost text-xl">Online Store</a>
+  </div>
+ 
+  <div className="navbar-end">
+    <button className="btn btn-ghost btn-xl">
+     Home
+    </button>
+    <button className="btn btn-ghost btn-xl">
+      <div className="indicator">
+        Cart
+        <span className="badge badge-xs badge-primary indicator-item">4</span>
+      </div>
+    </button>
+  </div>
+</div>
    
     
     </>
